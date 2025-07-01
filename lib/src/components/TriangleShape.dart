8a0cd22be101e9a5725fe62e5405e1cb7539ade1
hyewon6588 <hhan29@my.centennialcollege.ca>
2025-07-01 18:56:01 -0400

    merging code 02: draw circle around triangle

--- lib/src/components/TriangleShape.dart
+++ lib/src/components/TriangleShape.dart
@@ -23,9 +23,34 @@ class TriangleShape extends PositionComponent {
   }
 
   List<Vector2> getTriangleVertices() {
-    final top = Vector2(position.x + size.x / 2, position.y);
-    final bottomLeft = Vector2(position.x, position.y + size.y);
-    final bottomRight = Vector2(position.x + size.x, position.y + size.y);
+    final topLeft = absoluteTopLeftPosition;
+    final top = topLeft + Vector2(size.x / 2, 0);
+    final bottomLeft = topLeft + Vector2(0, size.y);
+    final bottomRight = topLeft + Vector2(size.x, size.y);
+
     return [top, bottomLeft, bottomRight];
+  }
+
+  bool isFullyEnclosedByUserPath(List<Vector2> userPath) {
+    // 사용자가 그린 경로가 삼각형 꼭짓점을 모두 포함하는지 검사
+    for (final v in getTriangleVertices()) {
+      if (!isPointInPolygon(v, userPath)) return false;
+    }
+    return true;
+  }
+
+  bool isPointInPolygon(Vector2 point, List<Vector2> polygon) {
+    int intersectCount = 0;
+    for (int i = 0; i < polygon.length; i++) {
+      Vector2 a = polygon[i];
+      Vector2 b = polygon[(i + 1) % polygon.length];
+
+      if (((a.y > point.y) != (b.y > point.y)) &&
+          (point.x <
+              (b.x - a.x) * (point.y - a.y) / (b.y - a.y + 0.0001) + a.x)) {
+        intersectCount++;
+      }
+    }
+    return intersectCount % 2 == 1;
   }
 }
