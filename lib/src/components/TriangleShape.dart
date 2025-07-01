4e810cc5c32dce1404acd9fdf20b10327ffb99e1
hyewon6588 <hhan29@my.centennialcollege.ca>
2025-07-01 15:59:10 -0400

    merging code 01: draw triangle

--- lib/src/components/TriangleShape.dart
+++ lib/src/components/TriangleShape.dart
@@ -4,12 +4,28 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class TriangleShape extends PositionComponent {
-  TriangleShape(Vector2 position) : super(position: position);
+  TriangleShape(Vector2 position)
+    : super(position: position, size: Vector2.all(60));
 
   @override
   void render(Canvas canvas) {
     final paint = Paint()..color = Colors.amber.shade100;
-    canvas.drawCircle(Offset(size.x / 2, size.y / 2), size.x / 2, paint);
+    final path = Path()
+      ..moveTo(size.x / 2, 0)
+      ..lineTo(0, size.y)
+      ..lineTo(size.x, size.y)
+      ..close();
 
+    canvas.save();
+    canvas.translate(0, 0);
+    canvas.drawPath(path, paint);
+    canvas.restore();
+  }
+
+  List<Vector2> getTriangleVertices() {
+    final top = Vector2(position.x + size.x / 2, position.y);
+    final bottomLeft = Vector2(position.x, position.y + size.y);
+    final bottomRight = Vector2(position.x + size.x, position.y + size.y);
+    return [top, bottomLeft, bottomRight];
   }
 }
