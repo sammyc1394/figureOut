3a350f8d1eb9881471d15825b4009fd49636ba10
sammyc1394 <codewithsam1394@gmail.com>
2026-03-26 09:22:10 +0900

    refactoring done

--- lib/src/components/PreparedEnemy.dart
+++ lib/src/components/PreparedEnemy.dart
@@ -3,30 +3,33 @@ import '../behaviors/shapeBehavior.dart';
 
 class PreparedEnemy {
   final String shapeType;
-  final int count;
+  final int energy;
   final bool isDark;
   final Vector2 actPosition;
+  final Vector2 customSize;
 
   final int? order;
   final double? attackTime;
+  final double? attackDamage;
   final ShapeBehavior? behavior;
 
   const PreparedEnemy({
     required this.shapeType,
-    required this.count,
+    required this.energy,
     required this.isDark,
     required this.actPosition,
     this.attackTime,
+    this.attackDamage,
     required this.order,
     required this.behavior,
-    Vector2? customSize,
+    required this.customSize,
   });
 
   @override
   String toString() {
     return 'PreparedEnemy('
         'shapeType: $shapeType, '
-        'count: $count, '
+        'energy: $energy, '
         'isDark: $isDark, '
         'actPosition: $actPosition, '
         'order: $order, '
